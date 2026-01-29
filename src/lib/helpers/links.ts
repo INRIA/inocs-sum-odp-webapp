586c6e8962e373563d44b3db6de57a71e6f8b16b
Rebeca MURILLO <rebeca.murillo.pro@gmail.com>
2026-01-29 12:24:04 +0100

    fix: dependency upgrade and process env fix link

--- src/lib/helpers/links.ts
+++ src/lib/helpers/links.ts
@@ -2,7 +2,7 @@ export function getUrl(path?: string) {
   if (path && path.startsWith("http")) {
     return path;
   }
-  const baseURl = process.env.BASE_URL ?? import.meta.env.BASE_URL ?? "";
+  const baseURl = import.meta.env.BASE_URL ?? process.env.BASE_URL ?? "";
   if (baseURl === "/") {
     return path ?? "#";
   }
